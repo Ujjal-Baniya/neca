1ca5362860e89ba5826ed6a9cc83bf034228f615
Ujjal Baniya <ujjalbaniya@gmail.com>
2025-04-22 21:06:20 -0600

    fixing for render

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+import os
 from app import create_app, socketio
 
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=app.config['DEBUG'], host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, debug=app.config['DEBUG'], host='0.0.0.0', port=port)
